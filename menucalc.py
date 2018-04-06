41f76d3d1debfedc4c03a91caa82fb0168a188b3
Andrew Ault <andrew@baxter.tamz.us>
2018-04-06 13:27:06 -0400

    cont. work

--- menucalc.py
+++ menucalc.py
@@ -2,14 +2,15 @@
 4/5/18
 '''
 userinput = ""
+manyinput = True
 print("Welcome to Menu Calculator! Press Enter to contiune.")
 input()
 print("There is five different options for what food you can order.")
 input()
-print("'1' Pizza - Small $8.99\n'2'Pizza - Medium $11.99\n'3'Pizza - Large $14.99\n'4'Breadsticks - Regular $4.99\n'5'2 Liter Soda - $3.99")
+print("'1'Pizza - Small $8.99\n'2'Pizza - Medium $11.99\n'3'Pizza - Large $14.99\n'4'Breadsticks - Regular $4.99\n'5'2 Liter Soda - $3.99")
 while userinput != "quit":
     print("When you are done ordering type quit and the program will move on")
-    userinput += input("Please type in the number next to the item you want: ")
+    userinput = input("Please type in the number next to the item you want: ")
     if userinput == "quit":
         break
     try:
@@ -19,5 +20,12 @@ while userinput != "quit":
             continue
     except:
         print("Oops! That is not a number!")
-        
-    
+        continue
+    while True:
+        howmany = input("How many?: ")
+        try:
+            howmany = int(howmany)
+            break
+        except:
+            print("Oof, not a number!")
+            continue
